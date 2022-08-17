13fbb3ae0f45b604a1930421dc229fbe091fcc91
DeMonLiu623 <757454343@qq.com>
2022-08-17 14:28:53 +0800

    feature:存储路径优可修改

--- app/src/main/java/com/demon/apport/ui/ChangePathActivity.kt
+++ app/src/main/java/com/demon/apport/ui/ChangePathActivity.kt
@@ -1,23 +1,20 @@
 package com.demon.apport.ui
 
 import android.Manifest
-import android.content.Intent
-import android.net.Uri
 import android.os.Build
 import android.os.Environment
-import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.documentfile.provider.DocumentFile
 import com.demon.apport.App
 import com.demon.apport.base.BaseActivity
 import com.demon.apport.data.Constants
 import com.demon.apport.databinding.ActivityChangePathBinding
 import com.demon.apport.service.WebHelper
-import com.demon.apport.util.*
-import com.demon.qfsolution.utils.getDataColumn
+import com.demon.apport.util.get
+import com.demon.apport.util.mmkv
+import com.demon.apport.util.put
+import com.demon.apport.util.toast
 import com.demon.qfsolution.utils.getExternalOrFilesDirPath
-import com.demon.qfsolution.utils.getFileFromUriN
-import com.demon.qfsolution.utils.grantPermissions
+import com.demon.qfsolution.utils.uriToFile
 import com.permissionx.guolindev.PermissionX
 import java.io.File
 
@@ -35,19 +32,8 @@ class ChangePathActivity : BaseActivity<ActivityChangePathBinding>() {
 
     private val openDocumentTree = registerForActivityResult(ActivityResultContracts.OpenDocumentTree()) {
         it ?: return@registerForActivityResult
-        val uri = Uri.parse(it.toString().replace("%3A", "/"))
-        runCatching {
-            val takeFlags = (intent.flags
-                    and (Intent.FLAG_GRANT_READ_URI_PERMISSION
-                    or Intent.FLAG_GRANT_WRITE_URI_PERMISSION))
-            contentResolver.takePersistableUriPermission(uri, takeFlags)
-            val documentFile = DocumentFile.fromTreeUri(this@ChangePathActivity, uri)
-            changePath = uri.getDataColumn() ?: ""
-            Log.i(TAG, "openDocumentTree: $changePath")
-            binding.tvChange.text = "修改后的路径：$changePath"
-        }.onFailure { e ->
-            e.printStackTrace()
-        }
+        changePath = it.uriToFile()?.absolutePath ?: ""
+        binding.tvChange.text = "修改后的路径：$changePath"
     }
 
 
@@ -57,7 +43,7 @@ class ChangePathActivity : BaseActivity<ActivityChangePathBinding>() {
         val def: String = App.appContext.getExternalOrFilesDirPath(Environment.DIRECTORY_DCIM)
         val nowPath = "当前存储路径：${mmkv.get(Constants.MMKV_STORAGE_PATH, def)}"
 
-        val permissions = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+        val permissions = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             arrayOf(Manifest.permission.MANAGE_EXTERNAL_STORAGE)
         } else {
             arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE)
@@ -68,9 +54,12 @@ class ChangePathActivity : BaseActivity<ActivityChangePathBinding>() {
             btnChange.setOnClickListener {
                 PermissionX.init(this@ChangePathActivity)
                     .permissions(*permissions)
+                    .onExplainRequestReason { scope, deniedList ->
+                        scope.showRequestReasonDialog(deniedList, "修改存储路径需要文件存储、管理权限", "好的", "取消")
+                    }
                     .request { allGranted, _, _ ->
                         if (!allGranted) {
-                            "没有存储权限~".toast()
+                            "没有文件存储、管理权限~".toast()
                         } else {
                             openDocumentTree.launch(null)
                         }
