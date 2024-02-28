5d0956a4b3b80ccc88eeee3df2702e63dc4dc2ce
DeMon <demonl@binarywalk.com>
2024-02-28 10:56:58 +0800

    fix:android14 bugs

--- app/src/main/java/com/demon/apport/base/BaseActivity.kt
+++ app/src/main/java/com/demon/apport/base/BaseActivity.kt
@@ -1,9 +1,16 @@
 package com.demon.apport.base
 
+import android.app.Activity
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.viewbinding.ViewBinding
 import com.demon.apport.R
 import com.demon.apport.util.inflateViewBinding
@@ -20,6 +27,7 @@ abstract class BaseActivity<VB : ViewBinding> : AppCompatActivity() {
     protected val TAG = this.javaClass.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setSystemBarStatus()
         super.onCreate(savedInstanceState)
         binding = inflateViewBinding(layoutInflater)
         setContentView(binding.root)
@@ -42,4 +50,27 @@ abstract class BaseActivity<VB : ViewBinding> : AppCompatActivity() {
 
 
     protected abstract fun initData()
+
+
+    private fun setSystemBarStatus(safeCutout: Boolean = false, navBar: Boolean = false) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, window.decorView).let { controller ->
+            //controller.hide(WindowInsetsCompat.Type.statusBars())
+            //controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            if (navBar) {
+                controller.show(WindowInsetsCompat.Type.navigationBars())
+            } else {
+                controller.hide(WindowInsetsCompat.Type.navigationBars())
+            }
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            val lp = window.attributes
+            lp.layoutInDisplayCutoutMode = if (!safeCutout) {
+                WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+            } else {
+                WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_NEVER
+            }
+            window.attributes = lp
+        }
+    }
 }
